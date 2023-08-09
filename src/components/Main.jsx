0a6ecc01dae713c84333583c419b74da4bed5c76
Mark <muchirimark2@gmail.com>
2023-08-10 01:55:43 +0300

    working on main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 export default function Main() {
     return (
-        <div className="">
-            <h1>Main content goes here</h1>
+        <div className="maincontainer">
+            <h1 className="maintitle">Fun facts about React</h1>
+            <ul className="mainfacts">
+                <li>Was firsr released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </div>
     )
 }
