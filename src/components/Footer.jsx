82d0adb9769b3bb5579174142bc0ff5043fb689e
Mark <muchirimark2@gmail.com>
2023-08-10 00:30:16 +0300

    clean templete

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-	return (
-		<footer className="footer">
-			<small className="small">
-				© 2021 Muchiri development. All rights reserved.
-			</small>
-		</footer>
-	);
-}
