0581c08fdbed6b59403a207796b0c270ae825250
Mark <muchirimark2@gmail.com>
2023-08-15 02:16:46 +0300

    - added and styled the main section. - styled the tittle and the list components. - learned the new `::marker` css for styling the bullet dots in unlisted list components

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,14 @@
 export default function Main() {
     return (
-        <div className="maincontainer">
+        <main>
             <h1 className="maintitle">Fun facts about React</h1>
             <ul className="mainfacts">
-                <li>Was firsr released in 2013</li>
+                <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
