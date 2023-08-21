a59d672d94994f3de7f29fd6a7016df0ef41a43e
Mark <muchirimark2@gmail.com>
2023-08-22 00:44:47 +0300

    - Setup the airbnb project - Added the assets needed and routed the components and image - Finished setting up the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
-import reacticon from "../assets/react-icon-small.png";
+import "./Navbar.css";
+import airbnblogo from "../assets/airbnb-logo.png";
+
 export default function Navbar() {
 	return (
-		<div className="nav">
-			{/* Logo */}
-			<div className="logo">
-				<img className="reacticon" src={reacticon} alt="" />
-				<h3 className="logotext">ReactFacts</h3>
-            </div>
-            {/* List */}
-            <div className="listsection">
-			<h3 className="navlist">React Course - Project 1</h3>
-            </div>
-		</div>
+		<>
+			<nav className="Navbar">
+				<a href="/">
+					<img src={airbnblogo} alt="airbnb logo" />
+				</a>
+			</nav>
+		</>
 	);
 }
