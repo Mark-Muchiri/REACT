bb7d1f6009a499c76f829f62d528c09451010ec8
Mark <muchirimark2@gmail.com>
2023-08-23 01:35:35 +0300

    added the challenge question

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import "./App.css";
 import Card from "./components/card/Card";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/nav/Navbar";
+
 /*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
 */
 
 export default function App() {
