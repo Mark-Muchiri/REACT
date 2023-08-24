01343c47a578bdbe01213b88bdff3408aff35054
Mark <muchirimark2@gmail.com>
2023-08-25 02:13:51 +0300

    - removed challedge question

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,6 @@ import Hero from "./components/hero/Hero";
 import Navbar from "./components/nav/Navbar";
 import cardData from "./data/data";
 
-// ! changed the country key to location
-/*
-*	Challenge:
-
-*	- import the array of data from data.js
-*	- map over the array to create <Card /> components
-*	- display the array of card components under the navbar
-*	(in place of the current <Card /> component)
-
-*	Note: We haven't styled the group of components yet, so they'll
-*	still be block elements, stacked vertically. We'll add styling later.
-*/
-
 export default function App() {
 	const data = cardData.map(function (item) {
 		return (
