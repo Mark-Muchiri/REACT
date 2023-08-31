259efc03ba074f6928501d835d01ca8361c24542
Mark <muchirimark2@gmail.com>
2023-09-01 00:13:08 +0300

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,20 @@ export default function App() {
 		// key={item.id} is just there to avoid a react error
 		return <Card key={item.id} item={item} />;
 	});
+	// This consoles all the data that's presented in `const = data`
+	// Tip: To see the console.log in vscode, use the extension 'Console Ninja'
 	console.log(data);
-
 
 	return (
 		<>
+			{/* This are the fonts being used in this project */}
 			<link
 				href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap"
 				rel="stylesheet"
 			></link>
 			<Navbar />
 			<Hero />
+			{/* This is the card prop being used. From here refer to the file Card.jsx to see the flow of data */}
 			<section className="card-list">{data}</section>
 		</>
 	);
